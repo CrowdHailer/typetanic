6a14dba925ef73acd90f4a6d3413e8e313c77815
Peter Saxton <peterhsaxton@gmail.com>
2015-06-04 08:07:03 +0100

    lil fix

--- lib/typetanic/email.rb
+++ lib/typetanic/email.rb
@@ -3,7 +3,7 @@ module Stash
   ItemError = Class.new(StandardError)
 
   def dump(item)
-    raise ItemError.new unless item.is_a?(self)
+    raise ItemError.new item unless item.is_a?(self)
     item.dump
   end
 end
